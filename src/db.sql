9f32cafccf8cb1607082ee3620087580b2eec457
droneinspection <abhishek2aa2@gmail.com>
2022-11-26 15:58:45 +0200

    removed duplicate flaws
    --

--- src/db.sql
+++ src/db.sql
@@ -68,8 +68,8 @@ CREATE TABLE IF NOT EXISTS "pages_file" ("id" integer NOT NULL PRIMARY KEY AUTOI
 CREATE TABLE IF NOT EXISTS "django_session" ("session_key" varchar(40) NOT NULL PRIMARY KEY, "session_data" text NOT NULL, "expire_date" datetime NOT NULL);
 CREATE TABLE IF NOT EXISTS "auth_user" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "password" varchar(128) NOT NULL, "last_login" datetime NULL, "is_superuser" bool NOT NULL, "username" varchar(150) NOT NULL UNIQUE, "last_name" varchar(150) NOT NULL, "email" varchar(254) NOT NULL, "is_staff" bool NOT NULL, "is_active" bool NOT NULL, "date_joined" datetime NOT NULL, "first_name" varchar(150) NOT NULL);
 INSERT INTO auth_user VALUES(1,'pbkdf2_sha256$180000$WlHKEo3U3oYs$Ay+e3iVp4RcpShYgLIo7pR9xzX5F2zhjBeOKNYDgWa4=','2020-07-18 18:53:51.637895',1,'admin','','admin@admin.gov',1,1,'2020-07-18 18:53:30.850279','');
-INSERT INTO auth_user VALUES(2,'pbkdf2_sha256$180000$TYSMLRe7zRCg$3zEKLYW5kn48O9JTmh5tvbbCrKOzfHGeeM6xGVAEZL8=','2020-07-18 18:54:48.549576',0,'bob','','',0,1,'2020-07-18 18:54:06.551643','');
-INSERT INTO auth_user VALUES(3,'pbkdf2_sha256$180000$XNnR1s8tCrTw$0c1ikgpTeNoOTXEz4ZO5QOtkUyFRfVjyNyjpMoXGUVc=','2020-07-18 18:54:41.483579',0,'alice','','',0,1,'2020-07-18 18:54:15.508656','');
+INSERT INTO auth_user VALUES(2,'pbkdf2_sha256$180000$TYSMLRe7zRCg$3zEKLYW5kn48O9JTmh5tvbbCrKOzfHGeeM6xGVAEZL8=','2020-07-18 18:54:48.549576',0,'sponge','','',0,1,'2020-07-18 18:54:06.551643','');
+INSERT INTO auth_user VALUES(3,'pbkdf2_sha256$180000$XNnR1s8tCrTw$0c1ikgpTeNoOTXEz4ZO5QOtkUyFRfVjyNyjpMoXGUVc=','2020-07-18 18:54:41.483579',0,'wonder','','',0,1,'2020-07-18 18:54:15.508656','');
 DELETE FROM sqlite_sequence;
 INSERT INTO sqlite_sequence VALUES('django_migrations',19);
 INSERT INTO sqlite_sequence VALUES('django_admin_log',2);
